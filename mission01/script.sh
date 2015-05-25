dc8344fff357bb392d03a0d3dd73a88ee5c00f64
sdurr <sdurr@e3r1p12.42.fr>
2015-05-25 11:31:56 +0200

    ad

--- mission01/script.sh
+++ mission01/script.sh
@@ -1,2 +1,2 @@
 docker build -t "hello/world:v42" .
-docker run -d -p 80 -ti "hello/world:v42"
+docker run -d --name=hello -p 80 -it "hello/world:v42"
--- mission02/script.sh
+++ mission02/script.sh
@@ -1,3 +1,2 @@
 docker build --tag="coucou:42" .
 docker run -d --hostname=42born2code -p 22 -it coucou:42
-
